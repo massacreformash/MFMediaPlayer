6e6c57e843e8960fbcea41818a189fa03c86324d
彭盛尧 <massacreformash@gmail.com>
2018-01-02 11:24:00 +0800

    pod init

---
+++ MediaPlayer/MediaPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MediaPlayer
+//
+//  Created by Aaron Peng on 2018/1/2.
+//  Copyright © 2018年 BUPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MediaPlayer/MediaPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MediaPlayer
+//
+//  Created by Aaron Peng on 2018/1/2.
+//  Copyright © 2018年 BUPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
